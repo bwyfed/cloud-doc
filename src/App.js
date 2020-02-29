a02bb3c8dae8eac5f3aa62f55a39f30d32ca0cbb
Bianwy <bianwy025@qq.com>
2020-02-29 21:33:37 +0800

    实现搜索框组件的功能和样式，引用图标库。

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileSearch from './components/FileSearch';
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>this is the left</h1>
+        <div className="col bg-danger left-panel">
+          <FileSearch
+            title="我的云文档"
+            onFileSearch={value => {
+              console.log(value);
+            }}
+          />
         </div>
-        <div className="col-9 bg-primary right-panel">
+        <div className="col bg-primary right-panel">
           <h1>this is the right</h1>
         </div>
       </div>
---
+++ src/components/FileSearch.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const FileSearch = ({ title, onFileSearch }) => {
+  const [inputActive, setInputActive] = useState(false);
+  const [value, setValue] = useState('');
+  let node = useRef(null);
+
+  const closeSearch = e => {
+    e.preventDefault();
+    setInputActive(false);
+    setValue('');
+  };
+  useEffect(() => {
+    const handleInputEvent = event => {
+      const { keyCode } = event;
+      if (keyCode === 13 && inputActive) {
+        onFileSearch(value);
+      } else if (keyCode === 27 && inputActive) {
+        closeSearch(event);
+      }
+    };
+    document.addEventListener('keyup', handleInputEvent);
+    return () => {
+      document.removeEventListener('keyup', handleInputEvent);
+    };
+  });
+  useEffect(() => {
+    if (inputActive) {
+      node.current.focus();
+    }
+  }, [inputActive]);
+  return (
+    <div className="alert alert-primary d-flex justify-content-between align-items-center">
+      {!inputActive && (
+        <>
+          <span className="doc-title">{title}</span>
+          <button
+            type="button"
+            className="icon-button"
+            onClick={() => setInputActive(true)}
+          >
+            <FontAwesomeIcon icon={faSearch} title="搜索" size="lg" />
+          </button>
+        </>
+      )}
+      {inputActive && (
+        <>
+          <input
+            className="form-control"
+            value={value}
+            ref={node}
+            onChange={e => {
+              setValue(e.target.value);
+            }}
+          />
+          <button type="button" className="icon-button" onClick={closeSearch}>
+            <FontAwesomeIcon title="关闭" size="lg" icon={faTimes} />
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FileSearch;
