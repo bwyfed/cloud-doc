2daa6f2fb05093d0569a75a9d9fdf2496190c77f
Bianwy <bianwy025@qq.com>
2020-03-12 15:49:10 +0800

    '实现基本的上下文菜单的弹出展示，基本的回调。'

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -4,7 +4,9 @@ import { faEdit, faTrash, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faMarkdown } from '@fortawesome/free-brands-svg-icons';
 import PropTypes from 'prop-types';
 import useKeyPress from '../hooks/useKeyPress';
-
+// load nodejs modules
+const { remote } = window.require('electron');
+const { Menu, MenuItem } = remote;
 const FileList = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
   const [editStatus, setEditStatus] = useState(false);
   const [value, setValue] = useState('');
@@ -20,6 +22,40 @@ const FileList = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
       onFileDelete(editItem.id);
     }
   };
+  useEffect(() => {
+    const menu = new Menu();
+    menu.append(
+      new MenuItem({
+        label: '打开',
+        click: () => {
+          console.log('clicking');
+        }
+      })
+    );
+    menu.append(
+      new MenuItem({
+        label: '重命名',
+        click: () => {
+          console.log('renaming');
+        }
+      })
+    );
+    menu.append(
+      new MenuItem({
+        label: '删除',
+        click: () => {
+          console.log('deleting');
+        }
+      })
+    );
+    const handleContextMenu = e => {
+      menu.popup({ window: remote.getCurrentWindow });
+    };
+    window.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      window.removeEventListener('contextmenu', handleContextMenu);
+    };
+  });
   useEffect(() => {
     const editItem = files.find(file => file.id === editStatus);
     if (enterPressed && editStatus && value.trim() !== '') {
